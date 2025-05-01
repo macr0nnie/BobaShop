4d2cd80805c7e3c082146a0ba3317f3f019d96c1
macronnie <macr0nnieasmr@gmail.com>
2025-05-01 11:32:48 -0400

    setting up the new component

--- src/app/drinks/graph/graph.component.ts
+++ src/app/drinks/graph/graph.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-graph',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './graph.component.html',
   styleUrl: './graph.component.css'
 })
 export class GraphComponent {
-
+  constructor(private employeeService: EmployeeService) {
+    this.fetchEmployees(); 
+  }
+  fetchEmployees() {
+    this.employeeService.getAllEmployees().subscribe(employees => {
+      console.log(employees);
+    });
+  }
 }
